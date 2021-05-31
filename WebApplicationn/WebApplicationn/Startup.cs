1a5ac0d215c0654a85f6cfbe0b7770864ec34d1a
197PM21993 - NGUYỄN ĐÌNH TRƯỜNG - K25T-PM1 <truong.197pm21993@vanlanguni.vn>
2021-05-31 11:18:50 +0700

    Add Database & Sanphams.controller & Loaisanpham

---
+++ WebApplicationn/WebApplicationn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationn.Startup))]
+namespace WebApplicationn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebApplicationnn/WebApplicationnn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationnn.Startup))]
+namespace WebApplicationnn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
